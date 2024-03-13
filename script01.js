04f4aab77b200626634e583596deac1d96c750f1
Rio <126503340+its-rio@users.noreply.github.com>
2024-03-13 17:55:18 +0530

    docs: start

---
+++ script01.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+// if else
+age = 10;
+
+if (age > 18){
+    console.log("You can vote....");
+}
+else if (age == 18){
+    console.log("You can vote....");
+}
+else{
+    console.log("You cannot vote ....");
+}
+
+// Ternery if else operator
+
+a = 20 ? console.log("hello") : console.log("Hii")
---
+++ script02.js
@@ -0,0 +1,13 @@
+// FOR loop
+for(var num = 1; num<=10; num++){
+    console.log(num * 14);
+}
+
+
+// WHILE loop
+var i = 1;
+while(i<=10){
+    // console.log(i * 5)
+    console.log("14 * ", i, "=", i * 5);
+    i++
+}
