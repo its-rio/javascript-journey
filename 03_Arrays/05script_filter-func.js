11642f88ee3b1057024d003ee10bad88665caec0
Rio <126503340+its-rio@users.noreply.github.com>
2024-03-29 12:02:13 +0530

    docs: adding some comments

--- 03_Arrays/05script_filter-func.js
+++ 03_Arrays/05script_filter-func.js
@@ -1,8 +1,21 @@
+// Filter method 
+    // - iterate on Array
+    // - perform operation on item to filter the item
+    // - Return the array which is returned after certain operation
 
+var words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
 
-const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
-
-const result = words.filter((word) => word.length > 6);
+var result = words.filter(function(item){
+    return( item.length > 6)
+});
 
 console.log(result);
 // Expected output: Array ["exuberant", "destruction", "present"]
+
+//  Even number find
+arr = new Array(1, 2, 3, 6, 5, 4); 
+var new_arr = arr.filter(function (x){ 
+    return x % 2==0; 
+}); 
+  
+console.log(new_arr) 
--- 03_Arrays/05script_reduce-func.js
+++ 03_Arrays/05script_reduce-func.js
@@ -1,4 +1,4 @@
-// Map method 
+// Reduce method 
     // - iterate on Array
     // - perform operation on item to reduce the value
     // - Return the array which is returned after certain operation
@@ -7,6 +7,6 @@ var arr = [2,6,8,15,12,31,24]
 
 var reduced = arr.reduce(function (sum, item){
     return(sum = sum * item)                // - Return the array which is returned after certain operation
-}, 0)
+}, 0)                                 // initial value of sum given here
 
 console.log(reduced);          // - Return the array which is returned after certain operation
